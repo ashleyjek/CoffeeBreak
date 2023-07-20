e81eaf3a7a3632cbc5a40d4cd0479697eb7a51da
ashley <ashleykimca@gmail.com>
2023-07-20 17:43:54 -0400

    user auth in progress

---
+++ backend/app/models/post.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  author_id  :bigint           not null
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+
+end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { restoreSession } from './store/csrf';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const initializeApp = () => {
+  const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+  let initialState = {};
+
+  if (currentUser) {
+    initialState = {
+      users: {
+        [currentUser.id]: currentUser
+      }
+    };
+  };
+};
+
+const store = configureStore();
+window.store = store;
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+restoreSession().then(initializeApp);
